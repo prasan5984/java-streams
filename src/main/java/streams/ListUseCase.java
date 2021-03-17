7c9ad8ec5bc873e611d458fa95e7ced4dea2db8d
prasanna <prasan5984@gmail.com>
2021-03-17 08:48:17 +0530

    ListUseCaseCollectorV2

--- src/main/java/streams/ListUseCase.java
+++ src/main/java/streams/ListUseCase.java
@@ -1,7 +1,15 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -15,8 +23,34 @@ public class ListUseCase {
     private static <T> List<T> toList(Stream<T> stream) {
         return stream
                 .parallel()
-                .collect(() -> new ArrayList<>(),
-                        (list, e) -> list.add(e),
-                        (l1, l2) -> l1.addAll(l2));
+                .collect(new Collector<T, List<T>, List<T>>() {
+                    @Override
+                    public Supplier<List<T>> supplier() {
+                        return ArrayList::new;
+                    }
+
+                    @Override
+                    public BiConsumer<List<T>, T> accumulator() {
+                        return List::add;
+                    }
+
+                    @Override
+                    public BinaryOperator<List<T>> combiner() {
+                        return (left, right) -> {
+                            left.addAll(right);
+                            return left;
+                        };
+                    }
+
+                    @Override
+                    public Function<List<T>, List<T>> finisher() {
+                        return Function.identity();
+                    }
+
+                    @Override
+                    public Set<Characteristics> characteristics() {
+                        return EnumSet.of(Characteristics.IDENTITY_FINISH);
+                    }
+                });
     }
 }
