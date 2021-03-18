e7fed9fdb00f1412b08238e133ff9c5855cb184c
prasanna <prasan5984@gmail.com>
2021-03-18 08:27:28 +0530

    CollectorUseCaseV2

--- src/main/java/streams/CollectorUseCase.java
+++ src/main/java/streams/CollectorUseCase.java
@@ -1,27 +1,21 @@
 package streams;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CollectorUseCase {
 
-    private static Map<String, List<String>> groupByDept(List<Student> list) {
+    private static Map<String, List<Student>> groupByDept(List<Student> list) {
         return list.stream()
-                   .collect(HashMap::new,
-                           (map, student) ->
-                                   map.computeIfAbsent(student.getDept(), s -> new ArrayList<>())
-                                      .add(student.getName()),
-                           (m1, m2) -> {
-                               for (Map.Entry<String, List<String>> entry : m1.entrySet())
-                                   m2.merge(entry.getKey(),
-                                           entry.getValue(),
-                                           (v1, v2) -> {
-                                               v1.addAll(v2);
-                                               return v1;
-                                           });
-                           });
+                   .collect(
+                           Collectors.groupingBy(
+                                   Student::getDept,
+                                   HashMap::new,
+                                   Collectors.toList()
+                           )
+                   );
     }
 
     private static class Student {
